41169d317f17e7a9859264b475d1014f81a2d522
Will Royal <william.j.royal@gmail.com>
2023-08-08 17:24:37 -0700

    initial commit

--- OperatorExercise/Program.cs
+++ OperatorExercise/Program.cs
@@ -4,7 +4,18 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int a = 17;
+            int b = 4;
+
+            int quotient = a / b;
+            int remainder = a % b;
+
+            if (a == 17 && b == 4)
+            {
+                Console.WriteLine($"17/4 is {quotient} remainder {remainder}. ");
+            }
+
+
         }
     }
 }
