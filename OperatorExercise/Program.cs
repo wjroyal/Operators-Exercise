230405622b9c55c3400660d3e35cec4575113ea2
Will Royal <william.j.royal@gmail.com>
2023-08-08 18:46:19 -0700

    final commit

--- OperatorExercise/Program.cs
+++ OperatorExercise/Program.cs
@@ -7,15 +7,25 @@
             int a = 17;
             int b = 4;
 
-            int quotient = a / b;
-            int remainder = a % b;
+            var div = a / b;
+            var mod = a % b;
 
-            if (a == 17 && b == 4)
-            {
-                Console.WriteLine($"17/4 is {quotient} remainder {remainder}. ");
-            }
+            
+            Console.WriteLine($"{a}/{b} is {div} remainder {mod}. ");
+       
+            //Allow user to input the radius of circle
+            Console.WriteLine("What is the radius of your circle?");
+            var radius = double.Parse(Console.ReadLine());
 
+            //Calculate Area
+            var areaOfCircle = AreaOfCircle(radius);
 
+            Console.WriteLine($"The area of a circle with radius of {radius} is {areaOfCircle}");
+
+        }
+      public static double AreaOfCircle(double radius)
+        {
+            return Math.PI * (radius * radius);
         }
     }
 }
